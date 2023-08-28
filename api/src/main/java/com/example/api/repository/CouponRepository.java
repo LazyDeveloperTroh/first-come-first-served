e8f64e524d32ef42b92e5ba14550218abc7c50be
troh <dhxofla91@gmail.com>
2023-08-28 22:33:22 +0900

    kafka producer까지 설정

---
+++ api/src/main/java/com/example/api/repository/CouponRepository.java
@@ -0,0 +1,9 @@
+package com.example.api.repository;
+
+import com.example.api.domain.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CouponRepository extends JpaRepository<Coupon, Long> {
+}
---
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -0,0 +1,31 @@
+package com.example.api.service;
+
+import com.example.api.domain.Coupon;
+import com.example.api.producer.CouponCreateProducer;
+import com.example.api.repository.CouponCountRepository;
+import com.example.api.repository.CouponRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApplyService {
+    private final CouponRepository couponRepository;
+
+    private final CouponCountRepository couponCountRepository;
+
+    private final CouponCreateProducer couponCreateProducer;
+
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer) {
+        this.couponRepository = couponRepository;
+        this.couponCountRepository = couponCountRepository;
+        this.couponCreateProducer = couponCreateProducer;
+    }
+
+    public void apply(Long userId) {
+        // redis incr, redis는 싱글 스레드로 동작
+        long count = couponCountRepository.increment();
+        if (count > 100) {
+            return;
+        }
+        couponCreateProducer.create(userId);
+    }
+}
